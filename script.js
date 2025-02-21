d01a1f0574e5cf89ea8493898b76f5f9c9b40520
kang.eroo <157279760+ykwong2002@users.noreply.github.com>
2025-02-21 11:41:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "****************************";
+const API_KEY = "AIzaSyC6rDJGsmv552VWNuUkbExZ6qEsgVAW0vE";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
